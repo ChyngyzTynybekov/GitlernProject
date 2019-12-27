5326a441cc16d2dc7667ff6dcd7b8945082da45a
ChyngyzTynybekov <ChyngyzTynybekov>
2019-12-26 20:58:58 -0600

    reverse string

---
+++ src/Stringg.java
@@ -0,0 +1,7 @@
+public class Stringg {
+    public static void main(String[] args) {
+       StringBuilder s= new StringBuilder();
+       s.append("Hello world");
+        System.out.println(s.reverse());
+    }
+}
